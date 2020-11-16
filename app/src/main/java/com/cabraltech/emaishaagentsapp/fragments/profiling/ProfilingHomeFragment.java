0eff5566558f54730d59e3aab11ffa97292e6753
ONENEMMANUEL <osoema.9@gmail.com>
2020-11-16 17:45:49 +0300

    Implemented validations on names and emails

--- app/src/main/java/com/cabraltech/emaishaagentsapp/fragments/profiling/ProfilingHomeFragment.java
+++ app/src/main/java/com/cabraltech/emaishaagentsapp/fragments/profiling/ProfilingHomeFragment.java
@@ -23,72 +23,47 @@ import android.widget.Toast;
 import com.cabraltech.emaishaagentsapp.R;
 import com.cabraltech.emaishaagentsapp.databinding.FragmentProfilingHomeBinding;
 
-
 public class ProfilingHomeFragment extends Fragment {
 
-    private  Context context;
+    private Context context;
     private FragmentProfilingHomeBinding binding;
     private NavController navController;
+
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         this.context = context;
     }
 
-
     public ProfilingHomeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_profiling_home,container,false);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Profiling");
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profiling_home, container, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Profiling");
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 
-        return  binding.getRoot();
+        return binding.getRoot();
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         navController = Navigation.findNavController(view);
 
-        binding.layoutProfilingFarmer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //navigation to step 1
-                navController.navigate(R.id.action_profilingHomeFragment_to_profilingFarmerFragment3)
-                ;
-
-            }
+        binding.layoutProfilingFarmer.setOnClickListener(v -> {
+            //navigation to step 1
+            navController.navigate(R.id.action_profilingHomeFragment_to_profilingFarmerFragment3);
         });
 
-        binding.layoutProfilingAssociation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.action_profilingHomeFragment_to_profilingAssociationFragment2);
-            }
-        });
+        binding.layoutProfilingAssociation.setOnClickListener(v -> navController.navigate(R.id.action_profilingHomeFragment_to_profilingAssociationFragment2));
 
-        binding.layoutAgroInputDealer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.action_profilingHomeFragment_to_profilingAgroInputDealersFragment2);
-            }
-        });
-        binding.layoutDashboardBulkBuyers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.action_profilingHomeFragment_to_profilingBulkBuyersFragment2);
-            }
-        });
+        binding.layoutAgroInputDealer.setOnClickListener(v -> navController.navigate(R.id.action_profilingHomeFragment_to_profilingAgroInputDealersFragment2));
 
-
+        binding.layoutDashboardBulkBuyers.setOnClickListener(v -> navController.navigate(R.id.action_profilingHomeFragment_to_profilingBulkBuyersFragment2));
     }
-
-
-
 }
