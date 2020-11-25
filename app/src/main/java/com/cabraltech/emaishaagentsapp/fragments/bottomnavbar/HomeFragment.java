4f2a8aa7dc89205e4c86f6b532ecd37c917a07a1
janepounds <janepounds1@gmail.com>
2020-11-25 12:18:13 +0300

    implemented gps permissions

--- app/src/main/java/com/cabraltech/emaishaagentsapp/fragments/bottomnavbar/HomeFragment.java
+++ app/src/main/java/com/cabraltech/emaishaagentsapp/fragments/bottomnavbar/HomeFragment.java
@@ -1,8 +1,15 @@
 package com.cabraltech.emaishaagentsapp.fragments.bottomnavbar;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.telephony.CarrierConfigManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +20,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -31,7 +39,9 @@ import com.google.android.material.snackbar.Snackbar;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
 
 import okhttp3.ResponseBody;
@@ -39,14 +49,16 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static android.content.Context.LOCATION_SERVICE;
 import static android.content.Context.MODE_PRIVATE;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements LocationListener{
     private static final String TAG = "HomeFragment";
 
-    private TextView textCommissions,temp,visibility,humidity,wind_speed,precipitation_type,precipitation;
+    private TextView textCommissions,temp,visibility,humidity,wind_speed,precipitation_type,precipitation,weather_day;
     LinearLayout profilingLayout, walletLayout, dataCollectionLayout, marketServicesLayout;
     private HomeViewModel homeViewModel;
+    private double latitude,longitude;
 
     public static final String WEATHER_API_KEY = "0CHwh88HI3G4GK62bu6glx6K4Nfxv6Uy";
 
@@ -61,10 +73,36 @@ public class HomeFragment extends Fragment {
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("eMaisha Agent");
         // ((AppCompatActivity)getActivity()).getSupportActionBar().getTitle().
+        getCooardinates();
+
 
         InitializeDashboard(view);
         return view;
 
+    }
+
+    public void getCooardinates() {
+        if ((ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) && (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            Log.d(TAG, "onCreateView: if");
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+        } else {
+            //pick coordinates
+            LocationManager locationManager = (LocationManager) requireContext().getSystemService(LOCATION_SERVICE);
+            boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (isGpsEnabled) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 10f, this);
+
+                if (locationManager != null) {
+                    latitude = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude();
+                    longitude = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude();
+                }
+
+
+            }
+            Log.d(TAG, "onCreateView: " + "latitude:" + latitude + " longitude:" + longitude);
+        }
     }
 
     public void InitializeDashboard(View view) {
@@ -79,9 +117,13 @@ public class HomeFragment extends Fragment {
         wind_speed = view.findViewById(R.id.wind_default);
         precipitation_type = view.findViewById(R.id.text_view_rain);
         precipitation = view.findViewById(R.id.rain_status);
+        weather_day = view.findViewById(R.id.weather_day);
 
 
         updateCommission();
+        Date now = new Date();
+        SimpleDateFormat simpleDateformat = new SimpleDateFormat("EEEE");
+        weather_day.setText(simpleDateformat.format(now));
 
         profilingLayout.setOnClickListener(v -> {
             Intent intent = new Intent(getContext(), ProfilingActivity.class);
@@ -116,7 +158,7 @@ public class HomeFragment extends Fragment {
 
 
         Call<WeatherResponse> call = APIClient.getWeatherInstance()
-                .getRealtimeWeather(WEATHER_API_KEY,0.3186962165260835f,32.55752216728469f,null,"si",fieldValues);
+                .getRealtimeWeather(WEATHER_API_KEY,(float)latitude,(float)longitude,null,"si",fieldValues);
         call.enqueue(new Callback<WeatherResponse>() {
             @Override
             public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
@@ -150,7 +192,8 @@ public class HomeFragment extends Fragment {
 
                     //set precipitation type
                     String precipitation_type_value = response.body().getPrecipitationType().getValue();
-                    precipitation_type.setText(precipitation_type_value);
+                    String precipitation_type_capitalized = precipitation_type_value.substring(0, 1).toUpperCase() + precipitation_type_value.substring(1);
+                    precipitation_type.setText(precipitation_type_capitalized);
 
 
 
@@ -196,5 +239,34 @@ public class HomeFragment extends Fragment {
                 Log.d(TAG, "onFailure: " + t.getMessage());
             }
         });
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1){
+            getCooardinates();
+
+        }
     }
 }
